91eac9cacb4221cce20fe69490acc2035f8812b0
Danielle Thompson <danithompson74@gmail.com>
2021-07-30 14:16:25 -0700

    Purposely fail 2nd test to save new albums & return all

--- spec/album_spec.rb
+++ spec/album_spec.rb
@@ -7,4 +7,15 @@ describe '#Album' do
       expect(Album.all).to(eq([]))
     end
   end
+
+  describe('#save') do
+    #covers two check - ability to save albums to mock db & verifies .all() works for multiple items
+    it("saves an album") do
+      album = Album.new("Giant Steps", nil)
+      album.save()
+      album2 = Album.new("Blue", nil)
+      album2.save()
+      expect(Album.all).to(eq([album, album2]))
+    end
+  end
 end
