2b586804ade09cc6e3cb66fa58d3b891efb07ed0
Danielle Thompson <danithompson74@gmail.com>
2021-08-18 14:28:51 -0700

    Add RESTful routing for songs

--- app.rb
+++ app.rb
@@ -5,6 +5,8 @@ require('sinatra')
 require('sinatra/reloader')
 require('./lib/album')
 require('pry')
+require('./lib/song')
+
 also_reload('lib/**/*.rb')
 #also_reload method tells app which files to reload
 #globbing pattern ** - looks inside all dirs inside lib
@@ -56,6 +58,44 @@ delete('/albums/:id') do
   erb(:albums)
 end
 
-get('/custom_route') do
-  #bonus routes as needed
+# details for a specific song
+get('/albums/:id/songs/:song_id') do
+  # find song by song id
+  @song = Song.find(params[:song_id].to_i())
+  erb(:song)
+end
+
+# post a new song & route back to album view
+post('/albums/:id/songs') do
+  # find album by album id
+  @album = Album.find(params[:id].to_i())
+  # create new song based on params of song name & album id
+  song = Song.new(params[:song_name], @album.id, nil)
+  song.save()
+  # return to album view
+  erb(:album)
+end
+
+# edit a song & route back to album view
+patch('/albums/:id/songs/:song_id') do
+  # finds album by id
+  @album = Album.find(params[:id].to_i())
+  # find the song needed for view
+  song = Song.find(params[:song_id].to_i())
+  # associate the updated song to the specific album found
+  song.update(params[:name], @album.id)
+  # redirect to album to which the song belongs
+  erb(:album)
+end
+
+# deletes a song & routes back to album view
+delete('/albums/:id/songs/:song_id') do
+  # finds song to be deleted
+  song = Song.find(params[:song_id].to_i())
+  # delete said song
+  song.delete()
+  # find the album associated with dead song
+  @album = Album.find(params[:id].to_i())
+  # reroutes back to album id found
+  erb(:album)
 end
